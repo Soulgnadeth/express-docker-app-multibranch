0e8b7448372368d88b2e10fc48716417fa4381e8
Soulgnadeth <toulpc9@gmail.com>
2025-10-15 16:59:33 +0700

    update commit

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { type Express, type Request, type Response } from 'express'
 
 const app: Express = express()
 
-const port: number = 3300
+const port: number = 3000
 
 // Routes
 // GET /
